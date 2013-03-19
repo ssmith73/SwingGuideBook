73685ba8cd09f134bea1e51e14c23ea3894a3816
Sean Smith <ssmith73@gmail.com>
2013-03-19 10:51:55 +0000

    Initial commit.

---
+++ SwingGuideBook/src/swingGuideBook/JFramesTest.java
@@ -0,0 +1,48 @@
+package swingGuideBook;
+import javax.swing.*;
+
+/*
+ *    --------Root Pane --(top level containers)------
+ *   |												  |
+ *   |	 JFrame, JDialog or JApplet					  |	
+ *   |												  |
+ *   |    -------------Layered Pane -----------------------
+ *   |   |			  								       |
+ *   |   |										           |
+ *   |   | 	   -------------Content Pane -----------------------
+ *   |   |	  |		 	  								        |
+ *   |   |	  |_MENU BAR__(not actually part of content pane)___|
+ *   |   |    |		 	  								        |
+ *   |   |    |		 	  								        |
+ *   |   |	  | All text, buttons and graphics go on this pane	|	
+ *    ---|	  |		(contains all visible components of root)								 		    |	
+ *       |	  |										 		  	|	
+ *        ----| 												|
+ * 			  | 												|
+ *             -------------------------------------------------
+ *               
+ *               AND A GLASS PANE OVER ALL 
+ *               
+ */
+public class JFramesTest {
+	private static void createAndShowGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame("[=] Hello World [=]");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//		frame.setSize(400,100);
+		frame.pack(); //useful only when layout managers are used
+		frame.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+
+}
